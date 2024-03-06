1f247947538ecf35b9986936cc347c78e9030f66
Gorji-Lavanya-18 <158832698+Gorji-Lavanya-18@users.noreply.github.com>
2024-03-06 19:34:41 +0530

    Create mulevel.cpp

---
+++ week5/mulevel.cpp
@@ -0,0 +1,32 @@
+// multilevel inheritance
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+public:
+    void vehicle () {
+        cout << "Vehicle engine started" << endl;
+    }
+};
+
+class Fourwheeler : public Vehicle {
+public:
+    void four() {
+        cout << "i am a four wheeler vehicle" << endl;
+    }
+};
+
+class Car : public Fourwheeler {
+public:
+    void car() {
+        cout << "car is a fourwheeler vehicle" << endl;
+    }
+};
+
+int main() {
+    Car obj;
+    obj.vehicle();
+    obj.four();
+    obj.car();
+    return 0;
+}
