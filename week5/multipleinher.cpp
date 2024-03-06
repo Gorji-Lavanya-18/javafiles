0f23c58f6a66af343c65a868ece2b2222feb2b6c
Gorji-Lavanya-18 <158832698+Gorji-Lavanya-18@users.noreply.github.com>
2024-03-06 19:39:39 +0530

    Create multipleinher.cpp

---
+++ week5/multipleinher.cpp
@@ -0,0 +1,22 @@
+//multiple inheritance
+#include<iostream>
+using namespace std;
+class Vehicle{
+    public:
+    void vehicle(){
+        cout<<"i am a vehicle"<<endl;
+    }
+};
+class Fourwheeler{
+    public:
+    void four(){
+        cout<<"i am a four wheeler vehicle"<<endl;
+    }
+};
+class car: public Vehicle, public Fourwheeler{};
+int main(){
+    car obj;
+    obj.vehicle();
+    obj.four();
+    return 0;
+}
