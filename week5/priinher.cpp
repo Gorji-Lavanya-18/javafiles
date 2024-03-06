d2d4a93bbb77e2933ae7689de366824045a6416f
Gorji-Lavanya-18 <158832698+Gorji-Lavanya-18@users.noreply.github.com>
2024-03-06 19:29:35 +0530

    Create priinher.cpp

---
+++ week5/priinher.cpp
@@ -0,0 +1,36 @@
+//private inheritance
+#include<iostream>
+using namespace std;
+class Parent{
+    public:
+    int publicVar=10;
+
+    private:
+    int privateVar=20;
+
+    protected:
+    int protectedVar=30;
+
+    public:
+    int getPrivateVar(){
+       return privateVar;
+    }
+};
+
+class Child : private Parent{
+    public:
+    int proTemp = protectedVar;
+    int pubTemp = publicVar;
+    int priTemp = getPrivateVar();
+};
+
+int main(){
+    Child obj;
+    cout<<"Public variable is "<<obj.pubTemp<<endl;
+
+    cout<<"Private Variable is "<<obj.priTemp<<endl;
+    //obj.getPrivateVar();
+    
+    //cout<<"Protected Variable is "<<obj.protectedVar<<endl;
+    cout<<"Protected Variable is "<<obj.proTemp<<endl;
+}
